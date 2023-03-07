8074be8864b97a873502f2a9d2e6603fc84dd995
MateoBaltar <mateobaltar@gmail.com>
2023-03-07 18:56:20 -0300

    Estancias ase

---
+++ Spring Security/estancias/src/main/java/com/estancias/estancias/EstanciasApplication.java
@@ -0,0 +1,13 @@
+package com.estancias.estancias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstanciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstanciasApplication.class, args);
+	}
+
+}
---
+++ Spring Security/estancias/src/test/java/com/estancias/estancias/EstanciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estancias.estancias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstanciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
